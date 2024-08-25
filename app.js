56b38c755326980d7bf0b337821c38e97f5088bc
MuhammadGhulamMuhiuddin <muhammadghulammuhiuddin791@gmail.com>
2024-08-25 13:34:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,295 @@
+// // Assignment 03
+
+// // Question 1
+// var age = ("I am 15 years old");
+// alert (age);
+
+// // Question 2
+// var visited = ("You have visited this site 14 times");
+// alert (visited);
+
+// // Question 3
+// var message = ("<h1>My birth year is 1990 <br/>  Data type of my Decleard variable is number<h1/>")
+// document.write("message");
+
+// // Question 4
+// document.write("John  Doe <span>ordered<span/> 5 T-Shirt(s) on XYZ clothing store");
+
+
+// // Assignmemnt 04
+
+// // Question 1
+ 
+// var Rules = ("Rules for naming JS Variable");
+// var rules = ("Rules for naming JS Variable");
+// var ruleS = ("Rules for naming JS Variable");
+
+// document.write(rules);
+
+// // Question 2
+
+// // Here are 5 Illegal Names are give below:
+
+// // var 123abc;
+// // var my-variable;
+// // var class;
+// // var my varible;
+// // var @myVar;
+
+// // Here are 5 legal Names are given below:
+// var myVariable;
+// var hidden_Value;
+// var $result;
+// var the_Rate_Of_Population;
+// var var1;
+
+// // Question 3
+// document.write("Variable name can only contain, number, $ and _. For Example: $my_1stVariable.");
+
+// document.write("Variable must begin with a letter. $ or _. For Example: $name, _name or name.");
+
+// document.write("Variable names are case sensitive.");
+
+// document.write("Variable names should not be JS keywords.");
+
+
+// // Assignment 5
+
+// // Question 1
+// document.write("Sum of 3 and 5 is =  ", 3 + 5 );
+// // Question 2
+// document.write("Sub of 3 and 5 is =  " , 3 - 5 );
+// document.write("Sub of 3 and 5 is =  "  ,3 * 5 );
+// document.write("Sub of 3 and 5 is =" , 3 / 5 );
+// document.write("Sub of 3 and 5 is = " , 3 % 5 );
+// // Question 3
+// // a
+// var num;
+// // b
+// var num1 = 12;
+// num1++;
+// // c
+// var num2 = 12;
+// ++num2;
+// // d
+// document.write("Initial Value is 5:");
+// var num3 = 5;
+// // e
+// num3++;
+// document.write(num3);
+// // f
+// var num4;
+// num4 = 5;
+// num4++;
+// document.write("The Value of after increment is", num4);
+// // g
+// document.write(num4 + 7);
+// // h
+// document.write("Value of After Addition is:" , 10 + 3);
+
+
+// // Question 5
+
+// document.write("Total  cost to buy 5 tickets to a movie is" , 600 * 5);
+// // Question 5 
+// // document.write("<h1>Table of 2<h1/>")
+// // var number = 4 ;
+// // document.write("number x 1 = " , number * 1 );
+// // document.write("number x 2 = " , number * 2 );
+// // document.write("number x 3 = " , number * 3 );
+// // document.write("number x 4 = " , number * 4 );
+// // document.write("number x 5 = " , number * 5 );
+// // document.write("number x 6 = " , number * 6 );
+// // document.write("number x 7 = " , number * 7 );
+// // document.write("number x 8 = " , number * 8 );
+// // document.write("number x 9 = " , number * 9 );
+// // document.write("number x 10 =" , number *  10);
+
+// Question 6
+
+ 
+
+
+// var celsius = 25; 
+
+
+// var fahrenheitFromCelsius = (celsius * 9/5) + 32;
+// document.write(`${celsius}°C is ${fahrenheitFromCelsius.toFixed(2)}°F<br>`);
+
+
+// var fahrenheit = 77; 
+
+
+// var celsiusFromFahrenheit = (fahrenheit - 32) * 5/9;
+// document.write(`${fahrenheit}°F is ${celsiusFromFahrenheit.toFixed(2)}°C`);
+
+// Question 7
+ 
+
+
+//  var priceOfItem1 = 15.99; 
+//  var priceOfItem2 = 29.99; 
+
+
+//  var quantityOfItem1 = 3; 
+
+//  var quantityOfItem2 = 2; 
+
+
+//  var shippingCharges = 5.99; 
+
+//  var totalCostOfItem1 = priceOfItem1 * quantityOfItem1;
+//  var totalCostOfItem2 = priceOfItem2 * quantityOfItem2;
+//  var subtotal = totalCostOfItem1 + totalCostOfItem2;
+//  var totalCost = subtotal + shippingCharges;
+
+
+//  document.write("<h2>Receipt</h2>");
+//  document.write("<p>Item 1: $" + priceOfItem1.toFixed(2) + " x " + quantityOfItem1 + " = $" + totalCostOfItem1.toFixed(2) + "</p>");
+//  document.write("<p>Item 2: $" + priceOfItem2.toFixed(2) + " x " + quantityOfItem2 + " = $" + totalCostOfItem2.toFixed(2) + "</p>");
+//  document.write("<p>Subtotal: $" + subtotal.toFixed(2) + "</p>");
+//  document.write("<p>Shipping Charges: $" + shippingCharges.toFixed(2) + "</p>");
+//  document.write("<p><strong>Total Cost: $" + totalCost.toFixed(2) + "</strong></p>");
+
+
+// Question 08
+
+        // var totalMarks = 500; 
+        // var marksObtained = 375; 
+
+
+        // var percentage = (marksObtained / totalMarks) * 100;
+
+        // document.write("<h2>Result</h2>");
+        // document.write("<p>Total Marks: " + totalMarks + "</p>");
+        // document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+        // document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
+
+
+// Question 9
+ 
+
+// var usdToPkrRate = 104.80;
+// var sarToPkrRate = 28;
+// var amountInUsd = 10;
+// var amountInSar = 25;
+
+
+// var totalInPkr = (amountInUsd * usdToPkrRate) + (amountInSar * sarToPkrRate);
+
+
+// document.write("Total in Pakistani Rupees: " + totalInPkr);
+
+
+// Question 10
+ 
+
+// var initialNumber = 20;
+
+
+// var result = (((initialNumber + 5) * 10) / 2);
+
+
+// document.write("<h1>Result: " + result + "</h1>");
+
+// Question 11
+        // var currentYear = 2024;
+        // var birthYear = 1990;
+
+     
+        // var ageIfBirthdayHasOccurred = currentYear - birthYear;
+
+       
+        // var ageIfBirthdayHasNotOccurred = ageIfBirthdayHasOccurred - 1;
+
+        // document.write("<h1>They are either " + ageIfBirthdayHasOccurred + " or " + ageIfBirthdayHasNotOccurred + " years old</h1>");
+
+// Question 12
+
+                // var radius = 10; 
+
+          
+                // var pi = 3.142;
+        
+                
+                // var circumference = 2 * pi * radius;
+        
+                // var area = pi * radius * radius;
+        
+              
+                // document.write("<h1>The circumference is " + circumference.toFixed(2) + "</h1>");
+                // document.write("<h1>The area is " + area.toFixed(2) + "</h1>");
+
+                // Question 13
+
+
+// var snack = "chocolates"; 
+
+
+// var currentAge = 25; 
+
+
+// var maxAge = 80; 
+
+
+// var amountPerDay = 2; 
+
+
+// var yearsLeft = maxAge - currentAge; 
+// var totalAmountNeeded = yearsLeft * 365 * amountPerDay; 
+
+
+// document.write("<h1>You will need " + totalAmountNeeded + " " + snack + " to last you until the ripe old age of " + maxAge + ".</h1>");
+
+
+
+
+
+
+
+
+// // Assignment 06
+// Question 1
+
+// document.write("<h1>Result<h1/>");
+// document.write("The value of a is: 10");
+// num = 10;
+// num++;
+// document.write("Now the value of is :", num);
+
+// document.write("The value of a is: 11");
+// num = 11;
+// num++;
+// document.write("Now the value of is :", num);
+
+// document.write("The value of a is: 11");
+// num = 12;
+// num--;
+// document.write("Now the value of is :", num);
+
+// document.write("The value of a is: 11");
+// num = 11;
+// --num;
+// document.write("Now the value of is :", num);
+
+// Question 2
+
+// var a = 2;
+// var b = 1;
+
+// var result = --a - --b + ++b + b--;
+
+//             1 - 0 + 2 + 0 = 3
+
+// console.log(result);
+
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+
+
+
+
+
